7054ac042d78bb57152d364c20ad596d61314551
nisalrenuja <nrenuja@gmail.com>
2023-07-26 16:23:16 +0530

    generate image filed added

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -13,6 +13,8 @@ const CreatePost = () => {
     prompt: "",
     photo: "",
   });
+
+  const generateImage = () => {};
   const handleSubmit = () => {};
   const handleChange = (e) => {};
   const handleSurpriseMe = () => {};
@@ -30,7 +32,7 @@ const CreatePost = () => {
       <form className="max-w-3xl mt-16" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-5">
           <FormField
-            label="Name"
+            labelName="Your Name"
             placeholder="Nisal Renuja Palliyaguru"
             value={form.name}
             handleChange={handleChange}
@@ -38,7 +40,7 @@ const CreatePost = () => {
             type="text"
           />
           <FormField
-            label="Prompt"
+            labelName="Prompt"
             placeholder="a pencil and watercolor drawing of a bright city in the future with flying cars"
             value={form.prompt}
             handleChange={handleChange}
@@ -47,6 +49,35 @@ const CreatePost = () => {
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="width-full height-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {generatingImg ? "Generating Image..." : "Generate Image"}
+          </button>
         </div>
       </form>
     </section>
